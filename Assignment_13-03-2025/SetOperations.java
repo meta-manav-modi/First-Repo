90a4a9f16069c3d13eccf02a53d03cdda77d5dec
manav modi <manav.modi@metacube.com>
2025-03-13 18:18:45 +0530

    Assisnment6

---
+++ Assignment_13-03-2025/SetOperations.java
@@ -0,0 +1,10 @@
+
+import intset.Utility;
+
+/** class Poly, execute the main function and call execute method */
+public class SetOperations {
+    public static void main(String[] args){
+        Utility utility = new Utility();
+        utility.execute();
+    }
+}
